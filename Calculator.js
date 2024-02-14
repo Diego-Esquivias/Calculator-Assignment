b37d1242be0c7391441efe5291f0f51fdff123d7
Diego-Esquivias <141285541+Diego-Esquivias@users.noreply.github.com>
2024-02-13 18:20:58 -0700

    Changed a lot

--- Calculator.js
+++ Calculator.js
@@ -1,88 +1,12 @@
-// Lines 3-99 add text to the calculator when a button is clicked
+var solution = ""
+var equation = ""
+// Lines 3-5 add text to the calculator when a button is clicked
 
-const seven = () => {
-    document.getElementById("textarea").value += "7"
+const addText = (text) => {
+    document.getElementById("textarea").value += text
 }
 
-const eight = () => {
-    document.getElementById("textarea").value += "8"
-}
-
-const nine = () => {
-    document.getElementById("textarea").value += "9"
-}
-
-const sqrt = () => {
-    document.getElementById("textarea").value += " √ "
-}
-
-const four = () => {
-    document.getElementById("textarea").value += "4"
-}
-
-const five = () => {
-    document.getElementById("textarea").value += "5"
-}
-
-const six = () => {
-    document.getElementById("textarea").value += "6"
-}
-
-const exp = () => {
-    document.getElementById("textarea").value += " ^ "
-}
-
-const one = () => {
-    document.getElementById("textarea").value += "1"
-}
-
-const two = () => {
-    document.getElementById("textarea").value += "2"
-}
-
-const three = () => {
-    document.getElementById("textarea").value += "3"
-}
-
-const sin = () => {
-    document.getElementById("textarea").value += " Sin "
-}
-
-const add = () => {
-    document.getElementById("textarea").value += " + "
-}
-
-const zero = () => {
-    document.getElementById("textarea").value += "0"
-}
-
-const sub = () => {
-    document.getElementById("textarea").value += " - "
-}
-
-const cos = () => {
-    document.getElementById("textarea").value += " Cos "
-}
-
-const multi = () => {
-    document.getElementById("textarea").value += " * "
-}
-
-const div = () => {
-    document.getElementById("textarea").value += " / "
-}
-
-const natLog = () => {
-    document.getElementById("textarea").value += " e "
-}
-
-const tan = () => {
-    document.getElementById("textarea").value += " Tan "
-}
-
-const log = () => {
-    document.getElementById("textarea").value += " Log "
-}
+// Lines 9-10 delete one character from the input tag
 
 const del = () => {
     let x = document.getElementById("textarea").value
@@ -90,29 +14,63 @@ const del = () => {
     document.getElementById("textarea").value = x
 }
 
+// Lines 17-20 clear everything from the input tag
+
 const clr = () => {
     document.getElementById("textarea").value = ""
 }
 
-const equal = () => {
-    let y = document.getElementById("textarea").value
-    y = y.split(" ")
+const sin = (text) => {
+    document.getElementById("textarea").value += text
+    equation = document.getElementById("textarea").value
+    equation = equation.split(" ")
+    solution = Math.sin(Number(equation[0]))
+    document.getElementById("textarea").value = solution
+}
 
-    for (let i = 0; i < y.length - 1; i++) {
-        let solution
-        if (y[i] == "+") {
-            solution = Number(y[i-1]) + Number(y[i+1])
-            document.getElementById("textarea").value = solution
-            y = y.splice(y[i-1], 3)
-            y = y.unshift(solution)
-            console.log(y)
-        } else if (y[i] == "-") {
-            solution = Number(y[i-1]) - Number(y[i+1])
-            document.getElementById("textarea").value = solution
-            y = y.splice(y[i-1], 3)
-            y = y.unshift(solution)
-            console.log(y)
-        }
-    }
+const cos = (text) => {
+    document.getElementById("textarea").value += text
+    equation = document.getElementById("textarea").value
+    equation = equation.split(" ")
+    solution = Math.cos(Number(equation[0]))
+    document.getElementById("textarea").value = solution
+}
+
+const tan = (text) => {
+    document.getElementById("textarea").value += text
+    equation = document.getElementById("textarea").value
+    equation = equation.split(" ")
+    solution = Math.tan(Number(equation[0]))
+    document.getElementById("textarea").value = solution
+}
+
+const log = (text) => {
+    document.getElementById("textarea").value += text
+    equation = document.getElementById("textarea").value
+    equation = equation.split(" ")
+    solution = Math.log10(Number(equation[0]))
+    document.getElementById("textarea").value = solution
+}
+
+const ln = (text) => {
+    document.getElementById("textarea").value += text
+    equation = document.getElementById("textarea").value
+    equation = equation.split(" ")
+    solution = Math.log(Number(equation[0]))
+    document.getElementById("textarea").value = solution
+}
+
+const sqrt = (text) => {
+    document.getElementById("textarea").value += text
+    equation = document.getElementById("textarea").value
+    equation = equation.split(" ")
+    solution = Math.sqrt(Number(equation[0]))
+    document.getElementById("textarea").value = solution
+}
+
+const equal = () => {
+        solution = document.getElementById("textarea").value
+        solution = eval(solution)
+        document.getElementById("textarea").value = solution
 }
 
